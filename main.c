cb0ddc5309a2f9f8e9fd2611caa0e3cc2765784a
Roberto Alcantara <roberto@eletronica.org>
2015-04-24 16:07:53 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: robertoalcantara
+ *
+ * Created on 24 de Abril de 2015, 16:00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Taih.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Taih.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=taih.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/taih.x.tar
